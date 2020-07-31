0a4f9b950806ad4825a45d570f3588b40c91e30f
Carlos Vinícius Ferrer Costa <carlosvferrer@gmail.com>
2020-07-31 04:31:26 -0300

    first commit

---
+++ sketch.js
@@ -0,0 +1,29 @@
+function preload() {
+  imagemDaEstrada = loadImage("/imagens/estrada.png");
+  imagemDoAtor = loadImage("/imagens/ator-1.png");
+  imagemCarro = loadImage("/imagens/carro-1.png");
+  imagemCarro2 = loadImage("/imagens/carro-2.png");
+  imagemCarro3 = loadImage("/imagens/carro-3.png");
+  imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3, imagemCarro, imagemCarro2, imagemCarro3];
+}
+
+function setup() {
+  createCanvas(500, 400);
+  somDaTrilhaSonora.loop();
+}
+
+function draw() {
+  background(imagemDaEstrada);
+  mostraAtor();
+  mostraCarro();
+  movimentaCarro();
+  movimentaAtor();
+  voltaPosicaoInicialDoCarro();
+  verificaColisao();
+  incluiPontos();
+  marcaPonto();
+}
+
+
+
+
